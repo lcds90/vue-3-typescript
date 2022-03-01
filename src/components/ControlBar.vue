5302edb268209920d6ad7b7b558f8ba7f7e1c125
lcds90 <lcds90@gmail.com>
2022-02-28 22:18:25 -0300

    finished lesson 4

---
+++ src/components/ControlBar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+// Emits have Literal type, only one valid string 
+const emit = defineEmits<{
+    (event: 'add-count', num: number): void;
+    (event: 'reset-count'): void;
+}>()
+
+</script>
+
+<template>
+
+<p>
+    <button @click="emit('add-count', 1)">Add 1</button>
+    <button @click="emit('reset-count')">Reset</button>
+</p>
+
+</template>
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,8 +1,19 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import fetchCount from '../services/fetchCount'
+import ControlBar from './ControlBar.vue'
 
+interface Props {
+  limit: number,
+  alertMessageOnLimit?: string,
+}
+
+// const props = defineProps<Props>()
+const props = withDefaults(defineProps<Props>(), {
+  alertMessageOnLimit: 'Max limit reached'
+})
 const count = ref<number | null>(null)
+const showAlert = ref<boolean>(false);
 
 onMounted(() => {
   fetchCount((initialCount) => {
@@ -12,7 +23,12 @@ onMounted(() => {
 
 function addCount(num: number) {
   if (count.value !== null) {
-    count.value += num
+    if(count.value >= props.limit){
+      showAlert.value = true
+    } else {
+      showAlert.value = false
+      count.value += num
+    }
   }
 }
 
@@ -21,6 +37,13 @@ function addCount(num: number) {
 <template>
   <p>{{ count }}</p>
   <p>
+    <p v-show="showAlert">{{props.alertMessageOnLimit}}</p>
     <button @click="addCount(1)">Add</button>
   </p>
+  <ControlBar
+    @add-count="addCount"
+    @reset-count="count = 0"
+  >
+
+  </ControlBar>
 </template>
