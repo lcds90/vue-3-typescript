6489599b31a6353ad6966e99754b3b0f595a2f06
lcds90 <lcds90@gmail.com>
2022-02-28 21:22:52 -0300

    lesson 3 finished

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, reactive, onMounted } from 'vue'
+import { ref, reactive, onMounted, computed } from 'vue'
 import fetchCount from './services/fetchCount'
 
 interface AppInfo {
@@ -9,10 +9,27 @@ interface AppInfo {
 
 const count = ref<number | null>(null)
 
+// type reference on nextCount
+// const nextCount = computed(() => {
+//   if(count.value !== null) return count.value + 1
+//   return null
+// })
+
 const appInfo: AppInfo = reactive({
   name: 'Counter',
   slogan: 'an app you can count on'
 })
+
+const addCount = (num: number) => {
+  if(count.value !== null){
+    count.value += num
+  }
+}
+
+// function dont have type reference, its typed in fetchCount
+// const callback = (initialCount) => {
+//   count.value = initialCount
+// }
 
 onMounted(() => {
 	fetchCount((initialCount) => {
